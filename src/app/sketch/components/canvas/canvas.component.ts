8b98b22c0f33b40b6cf0f9585ed8569dd52933ef
Gabriel Duarte <gabrield.developer@gmail.com>
2021-08-01 21:13:50 -0300

    fix(canvas): fixes canvas height and width setup

--- src/app/sketch/components/canvas/canvas.component.ts
+++ src/app/sketch/components/canvas/canvas.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { SketchService } from '../../services/sketch.service';
 
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.scss']
 })
-export class CanvasComponent implements OnInit {
+export class CanvasComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
+  private context: CanvasRenderingContext2D;
+
+  constructor(
+    private sketch: SketchService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    
+    this.context = this.canvas.nativeElement.getContext('2d');
+    this.updateCanvasSize();
+  }
+
+  private updateCanvasSize() {
+    console.log(this.canvas)
+    this.canvas.nativeElement.width = window.innerWidth;
+    this.canvas.nativeElement.height = window.innerHeight;
   }
 
 }
