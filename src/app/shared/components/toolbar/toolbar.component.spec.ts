405de279e4b95ac85c10c7378ada7ddce9c48633
Gabriel Duarte <gabrield.developer@gmail.com>
2021-08-23 23:27:09 -0300

    feat(toolbar): implements unit testing to components interaction to service

--- src/app/shared/components/toolbar/toolbar.component.spec.ts
+++ src/app/shared/components/toolbar/toolbar.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SketchService } from 'src/app/sketch/services/sketch.service';
 
 import { ToolbarComponent } from './toolbar.component';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
+  let sketchService: SketchService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,12 +16,53 @@ describe('ToolbarComponent', () => {
   });
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ToolbarComponent],
+      providers: [SketchService]
+    });
+
     fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    sketchService = TestBed.inject(SketchService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should increase pencil size', () => {
+    spyOn(sketchService, 'increaseSize');
+    
+    component.increasePencilSize();
+    expect(sketchService.increaseSize).toHaveBeenCalled();
+
+  });
+
+  it('should decrease pencil size', () => {
+    spyOn(sketchService, 'decreaseSize');
+    component.decreasePencilSize();
+    expect(sketchService.decreaseSize).toHaveBeenCalled();
+  });
+
+  it('should swap to pencil tool', () => {
+    spyOn(sketchService, 'swapToPencil');
+    component.usePencil();
+    expect(sketchService.swapToPencil).toHaveBeenCalled();
+  });
+
+  it('should swap to eraser tool', () => {
+    spyOn(sketchService, 'swapToEraser');
+    component.useEraser();
+    expect(sketchService.swapToEraser).toHaveBeenCalled();
+  });
+
+  it('should change current tool color', () => {
+    const color = "#000"
+    spyOn(sketchService, 'changeColor');
+    component.changeColor(color);
+    expect(sketchService.changeColor).toHaveBeenCalled();
+    expect(sketchService.color).toBe(color);
+  });
+
 });
